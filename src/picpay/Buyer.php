7ee4bd04268e3a7d76dad57d131eeb3ffa49dc7e
diogocqr <diogosantoscerqueira@gmail.com>
2021-08-01 16:51:32 -0300

    Classe Buyer criada

---
+++ src/picpay/Buyer.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace PicPay\Buyer;
+
+class Buyer{
+    /**
+     * @return array
+     */
+    public function returnBuyer(string $firstName, string $lastName, string $document, string $email, string $phone){
+        $buyerData = [
+            "firstName" => $firstName,
+            "lastName" => $lastName,
+            "document" => $document,
+            "email" => $email,
+            "phone => $phone"
+        ];
+        return (array) $buyerData;
+    }
+}
